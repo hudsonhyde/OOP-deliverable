c32fdb082fa44883833fa9ac73e3ea89262e0078
Hudson <Hudson_hyde1@baylor.edu>
2024-03-01 10:25:42 -0600

    finished code

--- StudentClass.py
+++ StudentClass.py
@@ -1,35 +1,38 @@
 from datetime import datetime
 class Student:
 
-    def __init__(self, sid, n, db, cl ):
+    def __init__(self, sid, n, dob, cl ):
         self.__studentID = sid
         self.__name = n
         self.__dob = dob
         self.__classification = cl
+        self.__age = 0
 
-    def calc_age(self, dob):
+    def calc_student_age(self):
         today = datetime.now().year
-        splitdob = dob.split('/')
-        current_age = today - splitdob[2]
+        splitdob = self.__dob.split('/')
+        year = int(splitdob[2])
+        age = today - year
+        return age
 
-
-    def get_current_age(current_age):
-        return current_age
     
-    def classification(self):
-        if self.__classification == 'senior'.lower():
-            print('Register 4/1 thru 4/3')
-        elif self.__classification == 'junior'.lower():
-            print('Register 4/4 thru 4/6')
-        elif self.__classification == 'sophomore'.lower():
-            print('Register 4/7 thru 4/9')
-        elif self.__classification == 'freshman'.lower():
-            print('Register 4/10 thru 4/12')
+    def registration(self):
+        if self.__classification == 'senior':
+            return 'Register 4/1 thru 4/3'
+        elif self.__classification == 'junior':
+            return 'Register 4/4 thru 4/6'
+        elif self.__classification == 'sophomore':
+            return 'Register 4/7 thru 4/9'
+        elif self.__classification == 'freshman':
+            return 'Register 4/10 thru 4/12'
         else:
-            print('not valid classisification')
+            return 'Not valid classification'
 
-    def get_classification(self):
-        return self.__classification
+    def get_registration(self):
+        return self.registration()
+    
+    def get_student_age(self):
+        return self.calc_student_age()
 
 
          
--- StudentProgram.py
+++ StudentProgram.py
@@ -1,2 +1,11 @@
 import StudentClass as sc 
 
+def main():
+
+    student = sc.Student('8911155', 'John', '02/28/1999', 'junior')
+
+    print('age:', student.get_student_age())
+
+    print(student.get_registration())
+
+main()
